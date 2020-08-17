5cb0b1ac2a4da548180835dd7e0dd5871f8d508d
Louis valder <louis_valder@hotmail.co.uk>
2020-08-17 12:10:08 +0100

    imported searchbar to app.js

--- src/container/app/App.js
+++ src/container/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "../../components/Header";
+import Searchbar from "../../components/Searchbar"
 import BookList from '../../components/BookList';
 
 function App() {
   return (
     <main>
       <Header />
+      <Searchbar />
       <BookList />
     </main>
   );
