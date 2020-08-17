0925a63607363e303dc65e06724c70c95350523f
Louis valder <louis_valder@hotmail.co.uk>
2020-08-17 13:05:07 +0100

    Completed props for searchbar

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,31 +1,18 @@
 import React, { Component } from "react";
 import styles from "./BookList.module.scss";
 import Book  from "../Book";
-import { firestore } from "../../firebase";
 
-class BookList extends Component {
-    state = {
-        Books:[]
-    }
+export default class BookList extends Component {
 
-    componentDidMount() {
-        firestore
-            .collection ("Books")
-            .get()
-            .then ((query) => {
-                const Books = query.docs.map(doc => doc.data ());
-                this.setState({ Books });
-            })
-        }       
 
-    render () {
+    render() {
+       
         return (
             <section className={styles.books}>
-                {this.state.Books.map((book, index) => (
-                    <Book bookData={book} key={index} />
-                ))}
+             {this.props.filteredbooks.map((books, index) => (
+                <Book bookData={books} key={index} />
+            ))}
             </section>
-        )
+        );            
     }
-}
-export default BookList
+} 
--- src/container/app/App.js
+++ src/container/app/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from "../../components/Header";
 import Searchbar from "../../components/Searchbar"
 import BookList from '../../components/BookList';
+import { firestore } from "../../firebase";
 
-function App() {
+export default class App extends Component {
+state = {
+  books: [],
+  searchText: "",
+  filteredbooks: []
+}
+
+setSearchText = (event) => {
+  const searchText = event.target.value;
+  this.setState({ searchText }, this.filterbooks);
+  
+}
+
+filterbooks = () => {
+  let filteredbooks = this.state.books.filter(book => {
+      return book.title
+      .toUpperCase()
+      .includes(this.state.searchText.toUpperCase());  
+  })
+  this.setState({ filteredbooks });
+}
+
+componentDidMount() {
+  firestore
+      .collection("Books")
+      .get()
+      .then((query) => {
+          const books = query.docs.map(doc => doc.data());
+          this.setState({ 
+              books: books,
+              filteredbooks: books 
+          });
+      })
+     
+}
+
+render () {
   return (
     <main>
       <Header />
-      <Searchbar />
-      <BookList />
+      <Searchbar searchText={this.state.searchText} setSearchText={this.setSearchText} />
+      <BookList filteredbooks={this.state.filteredbooks} />
     </main>
   );
 }
-
-export default App;
+} 
